ab3a22b45cb618f3f509b39f3ae35aeed05a82f5
Evan <evanmurray2012@gmail.com>
2018-11-14 17:18:26 +0000

    Update q2.pl

--- q2.pl
+++ q2.pl
@@ -1,1 +1,12 @@
+takes(tom, ct331).
+takes(mary, ct331).
+takes(joe, ct331).
+takes(tom, ct345).
+takes(mary, ct345).
+instructs(bob, ct331).
+instructs(ann, ct345).
+
+teaches(INSTRUCTOR, STUDENT):- takes(STUDENT, CLASS), instructs(INSTRUCTOR, CLASS).
+
+classmates(STUDENT1, STUDENT2):- takes(STUDENT1, CLASS), takes(STUDENT2, CLASS).
 
